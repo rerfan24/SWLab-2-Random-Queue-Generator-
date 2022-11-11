c19531ecfcdf81adaeb4b40e7ade027c66fc7868
rerfan24 <rezaerfan26@gmail.com>
2022-11-11 20:46:39 +0330

    Complete algorithm

--- FirstAlgorithm/Program.cs
+++ FirstAlgorithm/Program.cs
@@ -5,10 +5,44 @@ class Program
     {
         Random rnd = new Random();
         List<int> people = new List<int>();
-        for (int i = 0; i < 5; i++)
+        List<int> turn = new List<int>();
+
+        int remainingSeeds = 5;
+        for (int i = 0; i < remainingSeeds; i++)
         {
             people.Add(i + 1);
+            turn.Add(0);
         }
-        Console.WriteLine("[" + string.Join(", ", people) + "]");
+
+        int turnNumber = 1;
+        while (turnNumber < remainingSeeds + 1)
+        {
+            double seed = rnd.NextDouble();
+            double partition = (1 / (double)remainingSeeds);
+            double seedTop = partition;
+            for (int i = 0; i < remainingSeeds; i++)
+            {
+                if (seed < seedTop && turn[i] == 0)
+                {
+                    turn[i] = turnNumber;
+                    turnNumber++;
+                    break;
+                }
+                else if (seed < seedTop && turn[i] != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    seedTop += partition;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < remainingSeeds; i++)
+        {
+            System.Console.WriteLine(people[i] + "'s turn is: " + turn[i]);
+        }
     }
 }
